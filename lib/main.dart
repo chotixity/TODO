a070c128f04f2cbf64c63475b4e2697a2c57d2e3
chotixity <melkychoti@gmail.com>
2023-11-15 22:09:22 +0300

    a few changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/provider/tasks.dart';
-
 import './theme/dark.dart';
 import './widgets/bottombar.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  runApp(const TODOApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TODOApp extends StatelessWidget {
+  const TODOApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [ChangeNotifierProvider<Tasks>(create: (_) => Tasks())],
+      providers: [
+        ChangeNotifierProvider<Tasks>(create: (_) => Tasks()),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'Focus Bloom',
         theme: ThemeData(
             scaffoldBackgroundColor: Colors.white,
             textTheme: TextTheme(
